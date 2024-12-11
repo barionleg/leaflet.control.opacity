1715be0ed577c9c5459ea802fb0b73b9615f1b98
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-12-11 23:44:06 +0100

    Update mapp.js

--- docs/js/mapp.js
+++ docs/js/mapp.js
@@ -18,8 +18,9 @@ const o_std = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.pn
 //GSI Pale
 //const t_pale = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
 const t_pale = new L.tileLayer('https://{s}.basemaps.cartocdn.com/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-  }).addTo(map);
+attribution:
+        "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
+});
 
 //GSI Ort
 const t_ort = new L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg', {
