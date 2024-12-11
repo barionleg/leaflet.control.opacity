7bc3b9d337e7006abb18288fa544d48566bc4dab
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-12-11 23:00:26 +0100

    Create basemaplist.js

---
+++ src/basemaplist.js
@@ -0,0 +1,16 @@
+var baseMapNames = [
+  {"id":"rastertiles/voyager", "path":""},
+  {"id":"rastertiles/voyager_nolabels", "path":""},
+  {"id":"rastertiles/voyager_only_labels", "path":""},
+  {"id":"rastertiles/voyager_labels_under", "path":""},
+  {"id":"rastertiles/voyager_no_buildings", "path":""},
+  {"id":"rastertiles/voyager_only_labels_no_buildings", "path":""},
+  {"id":"rastertiles/voyager_no_labels_no_buildings", "path":""},
+  {"id":"light_all", "path":"light_omt/light_all-for-omt.tm2/project"},
+  {"id":"light_nolabels", "path":"light_omt/light_nolabels-for-omt.tm2/project"},
+  {"id":"light_only_labels", "path":"light_omt/light_onlylabels-for-omt.tm2/project"},
+  {"id":"dark_all", "path":"dark_omt/dark_all-for-omt.tm2/project"},
+  {"id":"dark_nolabels", "path":"dark_omt/dark_nolabels-for-omt.tm2/project"},
+  {"id":"dark_only_labels", "path":"dark_omt/dark_onlylabels-for-omt.tm2/project"},
+  {"id":"spotify_dark", "path":"spotify/spotify-dark.tm2/project"}
+];
